7d2ed7210655a83c61f986e705dfaf86927091b7
guteIdee <154327142+guteIdee@users.noreply.github.com>
2024-02-13 14:43:48 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var answer = 0
+    for i in 0 ..< signs.count {
+        answer += signs[i] == true ? absolutes[i] : -absolutes[i]
+    }
+    return answer
+}

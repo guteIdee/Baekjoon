4929d95858892c7ba8e16dc05136da464e4fcaaa
guteIdee <154327142+guteIdee@users.noreply.github.com>
2024-05-12 12:09:15 +0900

    [Gold V] Title: 강의실, Time: 364 ms, Memory: 49012 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -0,0 +1,26 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline().rstrip())  # 강의 갯수
+
+study = []
+for _ in range(n):
+    num, start, end = map(int, sys.stdin.readline().rstrip().split())
+    study.append([start, end])
+
+study.sort()
+
+rooms = []
+heapq.heappush(rooms, study[0][1])  
+
+for i in range(1, n):
+    if study[i][0] < rooms[0]:
+        heapq.heappush(rooms, study[i][1])
+    else:
+        heapq.heappop(rooms)
+        heapq.heappush(rooms, study[i][1])
+
+print(len(rooms))
+
+
+

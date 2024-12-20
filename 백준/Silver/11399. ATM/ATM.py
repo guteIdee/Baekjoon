5a2cebb06e193cb99659abc9330ac132841933b5
guteIdee <154327142+guteIdee@users.noreply.github.com>
2024-12-20 19:53:30 +0900

    [Silver IV] Title: ATM, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,13 +1,11 @@
-n = int(input())
+import sys
 
-times = list(map(int, input().split()))
-times.sort()
-
-total = 0
+n = int(sys.stdin.readline())
+l = list(map(int, sys.stdin.readline().split()))
+l.sort()
+time = 0
 answer = 0
-
-for i in times:
-    total += i
-    answer += total
-
+for i in l:
+    time += i
+    answer += time
 print(answer)

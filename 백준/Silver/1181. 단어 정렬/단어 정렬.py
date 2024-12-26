55d55d7396831e529b70ca4b0f6e3f42d4ebce83
guteIdee <154327142+guteIdee@users.noreply.github.com>
2024-12-26 20:50:09 +0900

    [Silver V] Title: 단어 정렬, Time: 3520 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,14 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = []
+l = []
 
 for _ in range(n):
-    arr.append(sys.stdin.readline().strip())
-arr = list(set(arr))
-arr.sort(key=lambda x: (len(x), x))
+    str = sys.stdin.readline().rstrip()
+    if str not in l:
+        l.append(str)
 
-for a in arr:
-    print(a)
+l.sort(key=lambda x:(len(x), x))
+
+for i in l:
+    print(i)
